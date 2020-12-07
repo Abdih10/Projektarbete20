d4aaea6b4aa8b18fa67029fa5503cadf057f68b5
Abdih10 <72185874+Abdih10@users.noreply.github.com>
2020-12-07 19:39:48 +0100

    All three test ok now!

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -5,6 +5,9 @@ public class StringCalculator {
 
         if (input.isEmpty()) {
             return 0;
+        }
+        if (input.length() == 1){
+            return Integer.parseInt(input);
         } else {
             return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
         }

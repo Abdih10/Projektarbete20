81405a69b8e6369c9e48c1f167a6f7bc97274328
Abdih10 <72185874+Abdih10@users.noreply.github.com>
2020-12-07 19:27:55 +0100

    Second test for a single number and should return same number.

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,16 +1,22 @@
-import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorTest {
+
     @Test
-    void emptyStringShouldReturnsZero(){
+    void emptyStringShouldReturnsZero() {
         StringCalculator calculator = new StringCalculator();
-
         var result = calculator.Add("");
-        assertEquals(0,result);
-
+        assertEquals(0, result);
     }
 
+    @Test
+    void singleNumberShouldReturnsSameNumber() {
+        StringCalculator calculator = new StringCalculator();
+
+        var result = calculator.Add("1");
+        assertEquals(1, result);
+    }
 }

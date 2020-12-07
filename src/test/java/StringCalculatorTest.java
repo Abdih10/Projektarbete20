518c6a310e6c65a8b764ba0e3ad49d18d12260ae
Abdih10 <72185874+Abdih10@users.noreply.github.com>
2020-12-07 19:19:44 +0100

    First test failed and i fixed the problem.

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,3 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringCalculatorTest {
+    @Test
+    void emptyStringShouldReturnsZero(){
+        StringCalculator calculator = new StringCalculator();
+
+        var result = calculator.Add("");
+        assertEquals(0,result);
+
+    }
 
 }
